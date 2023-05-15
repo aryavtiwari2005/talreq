1315f3f90f84377c8141af5b684e4efea01e5dbf
Aryav Tiwari <aryavtiwari2005@gmail.com>
2023-05-15 23:54:52 +0530

    'ee'

--- src/components/CompanyGoals.vue
+++ src/components/CompanyGoals.vue
@@ -3,24 +3,60 @@
         <div class="clients-helped" v-for="i in achievements" :key="i.id">
             <img :src="i.img">
             <h3>{{ i.title }}</h3>
-            <h2>{{ i.numbers }}</h2>
+            <div class="counter">
+                <h2 class="counter-value" :data-count="i.numbers">0</h2><h2>+</h2>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import $ from 'jquery'
 let id = 0;
+
 export default {
     name: 'CompanyGoals',
     data() {
         return {
             achievements: [
-                { id: id++, title: 'Clients Helped', numbers: '200+', img: 'https://media.discordapp.net/attachments/1023634058158276670/1105088926064054342/goals1.png'},
-                { id: id++, title: 'Candidates Placed', numbers: '2,000+', img: 'https://media.discordapp.net/attachments/1023634058158276670/1105088926248607764/goals2.png' },
-                { id: id++, title: 'Projects Completed', numbers: '500+', img: 'https://media.discordapp.net/attachments/1023634058158276670/1105088926500270260/goals3.png' },
-                { id: id++, title: 'Profiles Scouted', numbers: '700+', img: 'https://media.discordapp.net/attachments/1023634058158276670/1105088925854347294/goals4.png' }
+                { id: id++, title: 'Clients Helped', numbers: '200', img: 'https://media.discordapp.net/attachments/1023634058158276670/1105088926064054342/goals1.png' },
+                { id: id++, title: 'Candidates Placed', numbers: '2000', img: 'https://media.discordapp.net/attachments/1023634058158276670/1105088926248607764/goals2.png' },
+                { id: id++, title: 'Projects Completed', numbers: '500', img: 'https://media.discordapp.net/attachments/1023634058158276670/1105088926500270260/goals3.png' },
+                { id: id++, title: 'Profiles Scouted', numbers: '700', img: 'https://media.discordapp.net/attachments/1023634058158276670/1105088925854347294/goals4.png' }
             ]
         }
+    },
+    mounted() {
+        var a = 0;
+        $(window).scroll(function () {
+            var oTop = $('.company-goals').offset().top - window.innerHeight;
+            if (a == 0 && $(window).scrollTop() > oTop) {
+                $('.counter-value').each(function () {
+                    var $this = $(this),
+                        countTo = $this.attr('data-count');
+                    $({
+                        countNum: $this.text()
+                    }).animate({
+                        countNum: countTo
+                    },
+
+                        {
+
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function () {
+                                $this.text(Math.floor(this.countNum));
+                            },
+                            complete: function () {
+                                $this.text(this.countNum);
+                                //alert('finished');
+                            }
+
+                        });
+                });
+                a = 1;
+            }
+        });
     }
 }
 </script>
@@ -34,6 +70,12 @@ export default {
     width: 100%;
     align-items: center;
     justify-content: space-around;
+}
+
+.company-goals .clients-helped .counter-thing {
+    display: flex;
+    flex-direction: row;
+    margin: none;
 }
 
 .company-goals .clients-helped {
@@ -55,6 +97,7 @@ export default {
     margin: 0 0;
     font-size: 36px;
     font-weight: lighter;
+    display: inline;
 }
 
 .company-goals .clients-helped img {
